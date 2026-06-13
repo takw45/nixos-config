73e3774b46c0487ec2d554d5d124c54692f20ea5
takashi <3017297+takw45@users.noreply.github.com>
2026-06-14 00:59:17 +0900

    Update OS version => 26.05.

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "NixOS (COSMIC + home-manager)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
